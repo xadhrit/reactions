75aa9c5586355ef0de05e24643f29eb5d6b879ff
imadhrit <maharajrabel@gmail.com>
2021-07-14 15:48:00 +0530

    reactions

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StatusBar, Image, StyleSheet } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native"
+import { Block, GalioProvider } from 'galio-framework';
+import Screens from './navigation/Screens';
+import { Images,materialTheme } from './constants/';
+
+import { enableScreens } from "react-native-screens";
+enableScreens();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <GalioProvider theme={materialTheme}>
+           <Screens />
+          </GalioProvider>
+    </NavigationContainer>
   );
 }
 
